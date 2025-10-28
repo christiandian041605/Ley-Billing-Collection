b706789e19088ac14fc8a86a01dbe620a20524a9
Christian Dian Bona <christiandianbona@IO-2-MacBook-Air.local>
2025-10-28 11:26:06 +0800

    Fix CRUD errors: Disable error display in AJAX handlers
    - Changed display_errors from 1 to 0 in all process.php files
    - Changed display_startup_errors from 1 to 0
    - Changed error_reporting from E_ALL to 0
    - This prevents PHP errors from being output before JSON responses
    - Fixes 'An error occurred while updating' errors in AJAX requests
    - Affected files: responsibility_matrix, objectives, app_setting, users, sdp

--- users/process.php
+++ users/process.php
@@ -5,9 +5,9 @@ if (!isset($_SESSION['user_id'])) {
     echo json_encode(['success' => false, 'message' => 'Unauthorized access']);
     exit();
 }
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+ini_set("display_errors", 0);
+ini_set("display_startup_errors", 0);
+error_reporting(0);
 
 include_once "../config/database.php";
 include "../config/app.php";
